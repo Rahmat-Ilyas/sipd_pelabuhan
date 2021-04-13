29b60335749f81e1483b7f1cbf39226fe1d77b96
Rahmat Ilyas <rahmat.ilyas142@gmail.com>
2021-04-14 00:20:26 +0800

    perbaiki config

--- config.php
+++ config.php
@@ -1,29 +1,7 @@
 <?php
-// Midtrans
-// namespace Midtrans;
-
-// require('vendor/midtrans/midtrans-php/Midtrans.php');
-
-//Set Your server key
-// Config::$serverKey = "SB-Mid-server-ihGWw0dDF4hux9R5jAx7GeBd";
-
-// Uncomment for production environment
-// Config::$isProduction = true;
-
-// Enable sanitization
-// Config::$isSanitized = true;
-
-// Enable 3D-Secure
-// Config::$is3ds = true;
-
-// Uncomment for append and override notification URL
-// Config::$appendNotifUrl = "https://example.com";
-// Config::$overrideNotifUrl = "https://example.com";
-
-
 date_default_timezone_set("Asia/Makassar");
 session_start();
-$conn = mysqli_connect("localhost","rahmat_ryu","","db_pelabuhan");
+$conn = mysqli_connect("localhost","root","","db_pelabuhan");
 
 // Batalkan Reservasi
 $reservasi = mysqli_query($conn, "SELECT * FROM tb_transaksi WHERE status='Belum Lunas'");
